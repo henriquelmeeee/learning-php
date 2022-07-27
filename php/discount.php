90faa9d1273d0a92d0fd0a3a30048fc4c193691f
Henrique <86432400+henriquelmeeee@users.noreply.github.com>
2022-07-27 12:40:01 -0300

    Update discount.php

--- php/discount.php
+++ php/discount.php
@@ -1,3 +1,11 @@
 <?php
 $preco = (int)$_GET["valor"] - (int)$_GET["valor"] / 10;
 echo "Desconto de 10%: $preco";
+?>
+
+Outra forma:
+
+<?php
+$preco = (int)$_GET["valor"];
+$desconto = $preco / 10;
+echo "Desconto de 10%: ", $preco - $desconto;
