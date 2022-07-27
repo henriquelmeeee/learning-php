6017180d3c8927586d0896f8ca40a73ed7bca12d
Henrique <86432400+henriquelmeeee@users.noreply.github.com>
2022-07-27 12:24:16 -0300

    Update operators.php

--- php/operators.php
+++ php/operators.php
@@ -1,16 +1,4 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Curso PHP</title>
-</head>
-<body>
-    <?php
-        $valorum = (int)$_GET["a"];
-        $valordois = (int)$_GET["b"];
-        echo "A soma entre $valorum e $valordois é ", $valorum + $valordois
-    ?>
-</body>
-</html>
+<?php
+$valorum = (int)$_GET["a"];
+$valordois = (int)$_GET["b"];
+echo "A soma entre $valorum e $valordois é ", $valorum + $valordois
